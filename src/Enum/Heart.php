371a0604cf6a2717511e517c7ff32f5d7163c60f
Pierre du Plessis <pierre@pcservice.co.za>
2023-04-18 12:41:19 +0200

    Add Suite enums

---
+++ src/Enum/Heart.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Rsaweb\Poker\Enum;
+
+use Rsaweb\Poker\Contracts\Suite;
+
+enum Heart: int implements Suite
+{
+    case Ace = 1;
+    case Two = 2;
+    case Three = 3;
+    case Four = 4;
+    case Five = 5;
+    case Six = 6;
+    case Seven = 7;
+    case Eight = 8;
+    case Nine = 9;
+    case Ten = 10;
+    case Jack = 11;
+    case Queen = 12;
+    case King = 13;
+}
